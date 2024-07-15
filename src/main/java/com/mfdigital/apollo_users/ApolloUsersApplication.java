2472fa97b645e3bcd7868f042dee9ae73fa2e3f2
matheusponte <matheuspontes894@outlook.com>
2024-07-15 11:05:01 -0300

    :tada: Create base project

---
+++ src/main/java/com/mfdigital/apollo_users/ApolloUsersApplication.java
@@ -0,0 +1,13 @@
+package com.mfdigital.apollo_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloUsersApplication.class, args);
+	}
+
+}
