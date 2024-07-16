c2508b3ae33eb3f86c6700de93f00dc74e09144e
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-16 17:17:10 -0300

    FIX: coordinator model

---
+++ src/main/java/com/mfdigital/apollo_users/core/entity/Coordinator.java
@@ -0,0 +1,60 @@
+package com.mfdigital.apollo_users.core.entity;
+
+import com.mfdigital.apollo_users.core.entity.enums.Sector;
+import com.mfdigital.apollo_users.core.entity.enums.State;
+import com.mfdigital.apollo_users.core.entity.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Coordinator")
+public class Coordinator extends User{
+
+    @Id
+    private final String id_coordinator;
+
+    public Coordinator(String name, String lastName, Sector sector, State state, UserRole userRole, Boolean isActive, String email, String password) {
+        super(name, lastName, sector, state, userRole, isActive, email, password);
+        this.id_coordinator = super.id;
+    }
+
+    public String getId_coordinator() {
+        return id_coordinator;
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAutorization() {
+        return List.of();
+    }
+
+    @Override
+    public String getUserName() {
+        return name+" "+lastName;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Sector getSector() {
+        return sector;
+    }
+
+    @Override
+    public State getState() {
+        return state;
+    }
+}
