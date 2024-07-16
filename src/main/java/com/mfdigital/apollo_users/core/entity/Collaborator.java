faffa23a71e6cfee68f328b8c3e948a0c0b8629f
matheusponte <matheuspontes894@outlook.com>
2024-07-16 13:33:47 -0300

    Refact: Autorhization interface

--- src/main/java/com/mfdigital/apollo_users/core/entity/Collaborator.java
+++ src/main/java/com/mfdigital/apollo_users/core/entity/Collaborator.java
@@ -1,13 +1,18 @@
 package com.mfdigital.apollo_users.core.entity;
 
+import com.mfdigital.apollo_users.core.entity.enums.State;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 
-public class Collaborator extends UserClass {
+public class Collaborator extends User {
     private Timestamp tma;
 
     public Collaborator(Timestamp tma) {
+        super();
         this.tma = tma;
     }
 
@@ -17,5 +22,35 @@ public class Collaborator extends UserClass {
 
     public void setTma(Timestamp tma) {
         this.tma = tma;
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAutorization() {
+        return List.of();
+    }
+
+    @Override
+    public String getUserName() {
+        return "";
+    }
+
+    @Override
+    public String getEmail() {
+        return "";
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public Sector getSector() {
+        return null;
+    }
+
+    @Override
+    public State getState() {
+        return null;
     }
 }
