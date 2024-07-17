1734660eb7355c9f2e815f9de45cbbea69599829
matheusponte <matheuspontes894@outlook.com>
2024-07-17 07:36:10 -0300

    Fixrefat: Modificação de metodos

--- src/main/java/com/mfdigital/apollo_users/core/entity/Collaborator.java
+++ src/main/java/com/mfdigital/apollo_users/core/entity/Collaborator.java
@@ -1,29 +1,32 @@
 package com.mfdigital.apollo_users.core.entity;
 
+import com.mfdigital.apollo_users.core.entity.enums.Sector;
 import com.mfdigital.apollo_users.core.entity.enums.State;
-import lombok.Data;
+import com.mfdigital.apollo_users.core.entity.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.List;
 
+@Entity
+@Table(name = "Collaborator")
+
 public class Collaborator extends User {
+
     private Timestamp tma;
+    @Id
+    private final String id_collaborator;
 
-    public Collaborator(Timestamp tma) {
-        super();
-        this.tma = tma;
+    public Collaborator(String name, String lastName, Sector sector, State state, UserRole userRole, Boolean isActive, String email, String password) {
+        super(name, lastName, sector, state, userRole, isActive, email, password);
+        this.id_collaborator = super.id;
     }
 
-    public Timestamp getTma() {
-        return tma;
-    }
-
-    public void setTma(Timestamp tma) {
-        this.tma = tma;
-    }
-
+    public String getId_collaborator(){return id_collaborator}
     @Override
     public Collection<GrantedAuthority> getAutorization() {
         return List.of();
@@ -31,26 +34,26 @@ public class Collaborator extends User {
 
     @Override
     public String getUserName() {
-        return "";
+        return name + lastName;
     }
 
     @Override
     public String getEmail() {
-        return "";
+        return email;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public Sector getSector() {
-        return null;
+        return sector;
     }
 
     @Override
     public State getState() {
-        return null;
+        return state;
     }
 }
