c2ed5707f9b57dc9201349e4340312dfa190bf29
ramosmanel <emanuelramos20004@gmail.com>
2024-07-19 12:51:56 -0300

    FEAT: created execeptions class

---
+++ src/main/java/com/mfdigital/apollo_users/core/exceptions/CoordinatorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mfdigital.apollo_users.core.exceptions;
+
+public class CoordinatorNotFoundException extends RuntimeException{
+    public CoordinatorNotFoundException (String message) {
+        super(message);
+    }
+}
