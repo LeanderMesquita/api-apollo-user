766181f688a6c81f99a9c1e3afc6be563f7e38ab
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2024-07-23 09:32:41 -0300

    feat: create exceptions

---
+++ src/main/java/com/mfdigital/apollo_users/core/exceptions/CollaboratorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mfdigital.apollo_users.core.exceptions;
+
+public class CollaboratorNotFoundException extends RuntimeException {
+    public CollaboratorNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mfdigital/apollo_users/core/exceptions/SupervisorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mfdigital.apollo_users.core.exceptions;
+
+public class SupervisorNotFoundException extends RuntimeException {
+    public SupervisorNotFoundException(String message) {
+        super(message);
+    }
+}
