095590a0828f232763b5effcac7c68b651e110ee
matheusponte <matheuspontes894@outlook.com>
2024-07-25 08:11:02 -0300

    Fix: Methods and end points

--- src/main/java/com/mfdigital/apollo_users/core/http/controller/AdminController.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/controller/AdminController.java
@@ -16,77 +16,51 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/users/admin")
 public class AdminController {
     private final AdminService adminService;
 
     @Autowired
     public AdminController(AdminService adminService){
-        this.adminService = adminService
+        this.adminService = adminService;
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<Coordinator>> getAllACoordinators(){
-        List<Coordinator> admin = adminService.getAllCoordinators();
-        return  new ResponseEntity<>(admin, HttpStatus.OK);
+    @GetMapping("/view")
+    public ResponseEntity<List<Admin>> getAllAdmin() {
+        List<Admin> admin = adminService.getAllAdmin();
+        return new ResponseEntity<>(admin, HttpStatus.OK);
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<Collaborator>> getAllCollaborators(){
-        List<Collaborator> admin = adminService.getAllCollaborators();
-        return  new ResponseEntity<>(admin, HttpStatus.OK);
+    @GetMapping("/view/{id}")
+    public ResponseEntity<Admin> getAdminById(String id) {
+        Optional<Admin> admin = adminService.getAdminById(id);
+        return admin.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/")
-    public ResponseEntity<List<Supervisor>> getAllSupervisors(){
-        List<Supervisor> admin = adminService.getAllSupervisors();
-        return  new ResponseEntity<>(admin, HttpStatus.OK);
+    @PostMapping("/create")
+    public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) {
+        Admin newAdmin = adminService.createAdmin(admin);
+        return new ResponseEntity<>(newAdmin, HttpStatus.OK);
     }
 
-    @GetMapping("/")
-    public ResponseEntity<Coordinator> findCoordenatorById(@PathVariable String id){
-        Coordinator getCoordenatorById = adminService.findCoordinatorById(id);
-        return ResponseEntity.ok(getCoordenatorById);
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<Supervisor> getSupervisorsById(@PathVariable String id){
-        Supervisor getSupervisorsById = adminService.findSupervisorById(id);
-        return ResponseEntity.ok(getSupervisorsById);
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<Coordinator> createCoordinator(@RequestBody Coordinator coordenator){
-        Coordinator newCoordinator = adminService.createCoordinator(coordenator);
-        return new ResponseEntity<>(newCoordinator, HttpStatus.OK);
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<Collaborator> createCollaborator(@RequestBody Collaborator collaborato){
-        Collaborator newCollaborator = adminService.createCollaborator(collaborato);
-        return new ResponseEntity<>(newCollaborator, HttpStatus.OK);
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<Supervisor> createSupervisor(@RequestBody Supervisor supervisor){
-        Supervisor newSupervisor = adminService.createSupervisor(supervisor);
-        return new ResponseEntity<>(newSupervisor, HttpStatus.OK);
-    }
-
-
-
-    @PutMapping("/")
-    public ResponseEntity<Admin> updateAdmin(@PathVariable String id, @RequestBody Admin admin {
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Admin> updateAdmin(@PathVariable String id, @RequestBody Admin admin){
         Optional<Admin> existingAdmin = adminService.getAdminById(id);
-        if (existingAdmin.isPresent()){
+        if(existingAdmin.isPresent()) {
             Admin updateAdmin = existingAdmin.get();
-            BeanUtils.copyProperties(admin,updateAdmin,"id");
+            BeanUtils.copyProperties(admin, updateAdmin, "id", "status");
 
-            Admin updateAdmin = adminService.updateAdmin(id,updateAdmin);
+            Admin updateAdmin = adminService.updateAdmin(id, updateAdmin);
             return ResponseEntity.ok(updateAdmin);
-
-        }else {
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PutMapping("/status/{id}")
+    public ResponseEntity<Admin> inactivateAdmin(@PathVariable String id) {
+        Admin inactivateAdmin = adminService.inactivateAdmin(id);
+        return ResponseEntity.ok(inactivateAdmin);
+    }
+
 }
