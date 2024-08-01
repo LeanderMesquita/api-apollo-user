9aba67c54374bd0d451a383a90cd43ca58011637
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-01 17:05:14 -0300

    FEAT: Auth DTO

---
+++ src/main/java/com/mfdigital/apollo_users/core/http/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.mfdigital.apollo_users.core.http.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<>
+
+
+    @PostMapping("/register")
+
+}
