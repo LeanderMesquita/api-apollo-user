8eb99dfe057a6266e7a0474e2aa91eb0074a2b59
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-01 15:57:36 -0300

    FEAT: internal token filter configuration

---
+++ src/main/java/com/mfdigital/apollo_users/core/http/config/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.mfdigital.apollo_users.core.http.config.security;
+
+import com.mfdigital.apollo_users.core.http.services.TokenService;
+import com.mfdigital.apollo_users.core.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if (token != null){
+            var login = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByEmail(login);
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    public String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return "Token not provided or null";
+        return authHeader.replace("Bearer", "");
+    }
+}
