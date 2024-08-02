dce304316237ab3907334bcc4404921bfc9c7e0c
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-02 16:24:29 -0300

    FEAT: role validations and permissions

--- src/main/java/com/mfdigital/apollo_users/core/http/services/UserService.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/services/UserService.java
@@ -5,9 +5,12 @@ import com.mfdigital.apollo_users.core.http.DTO.UserRequestDTO;
 import com.mfdigital.apollo_users.core.repositories.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -25,11 +28,36 @@ public class UserService {
     }
 
     public User updateUser(String id, UserRequestDTO userDetails) {
-        User user = userRepository.findById(id)
+
+        User authUserWhoRequestingChange = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User userToUpdate = userRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("User not found."));
 
-        user.setUsername(userDetails.name()+ " "+userDetails.lastName());
-        return userRepository.save(user);
+        var roleAuthUser = authUserWhoRequestingChange.getUserRole().toString();
+        var roleUpdateUser = userToUpdate.getUserRole().toString();
+
+        var sectorAuthUser = authUserWhoRequestingChange.getSector().toString();
+        var sectorUpdateUser = userToUpdate.getSector().toString();
+
+        if(!Objects.equals(sectorAuthUser, sectorUpdateUser)){
+            throw new AccessDeniedException("You can only update users with the same sector");
+        }
+
+
+        if (
+                (roleUpdateUser.equals("ADMIN") || roleUpdateUser.equals("COORDINATOR"))
+                        &&
+                (roleAuthUser.equals("COORDINATOR") || roleAuthUser.equals("SUPERVISOR"))
+        )
+        {
+            throw new AccessDeniedException("Coordinators or supervisor cannot update admin users");
+        }
+
+
+
+        userToUpdate.setUsername(userDetails.name()+ " "+userDetails.lastName());
+        return userRepository.save(userToUpdate);
     }
 
     public User inactivateUser(String id) {
