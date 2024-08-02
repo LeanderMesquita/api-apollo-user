c2548a235d9ffda2920597350aa2e197c97bf511
ramosmanel <emanuelramos20004@gmail.com>
2024-08-02 02:09:57 -0300

    REFACTOR: specifying data

--- src/main/java/com/mfdigital/apollo_users/core/http/DTO/RegisterDTO.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/DTO/RegisterDTO.java
@@ -3,26 +3,36 @@ package com.mfdigital.apollo_users.core.http.DTO;
 import com.mfdigital.apollo_users.core.entity.enums.Sector;
 import com.mfdigital.apollo_users.core.entity.enums.State;
 import com.mfdigital.apollo_users.core.entity.enums.UserRole;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record RegisterDTO(
-        @NotBlank
-        @Pattern(regexp = "^[A-Za-z]", message = "Please, enter your name correctly.")
+        @NotBlank(message = "Name cannot be blank")
+        @Pattern(regexp = "^[A-Za-z]+$", message = "Please, enter your name correctly.")
         String name,
-        @NotBlank
-        @Pattern(regexp = "^[A-Za-z]", message = "Please, enter your last name correctly.")
+
+        @NotBlank(message = "Last name cannot be blank")
+        @Pattern(regexp = "^[A-Za-z]+$", message = "Please, enter your last name correctly.")
         String lastName,
-        @NotBlank
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Email must be valid and end with '@meirelesefreitas.adv.br'")
         @Pattern(regexp = "^[A-Za-z0-9.]+@meirelesefreitas\\.adv\\.br$", message = "Email must end with '@meirelesefreitas.adv.br'.")
         String email,
-        @Pattern(regexp = "^[A-Za-z0-9._%+#$&*@!?-]")
+
+        @NotBlank(message = "Password cannot be blank")
+        @Pattern(regexp = "^[A-Za-z0-9._%+#$&*@!?-]+$", message = "Password must contain only allowed characters")
         String password,
-        @NotBlank
+
+        @NotNull(message = "Role cannot be null")
         UserRole role,
-        @NotBlank
+
+        @NotNull(message = "Sector cannot be null")
         Sector sector,
-        @NotBlank
+
+        @NotNull(message = "State cannot be null")
         State state
 ) {
 }
