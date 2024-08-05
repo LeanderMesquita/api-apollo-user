42424dec24323bc1d228ba11edad1a2818c49e41
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-05 13:35:56 -0300

    FEAT: validator service

--- src/main/java/com/mfdigital/apollo_users/core/http/services/ValidatorService.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/services/ValidatorService.java
@@ -1,2 +1,38 @@
-package com.mfdigital.apollo_users.core.http.services;public class ValidatorService {
+package com.mfdigital.apollo_users.core.http.services;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class ValidatorService {
+
+    public void roleValidate(String roleUpdateUser, String roleAuthUser){
+
+        if(roleAuthUser.equals("COLLABORATOR")){
+            throw new AccessDeniedException("Collaborators cannot update others users");
+        }
+
+        if (
+                (roleUpdateUser.equals("ADMIN") || roleUpdateUser.equals("COORDINATOR"))
+                        &&
+                        (roleAuthUser.equals("COORDINATOR") || roleAuthUser.equals("SUPERVISOR"))
+        )
+        {
+            throw new AccessDeniedException("Coordinators or supervisor cannot update admin or coordinator users");
+        }
+
+
+        if(roleUpdateUser.equals("SUPERVISOR") && roleAuthUser.equals("SUPERVISOR")){
+            throw new AccessDeniedException("Supervisors cannot update others supervisors");
+        }
+    }
+
+    public void sectorValidate(String sectorUpdateUser, String sectorAuthUser){
+        if(!Objects.equals(sectorAuthUser, sectorUpdateUser)){
+            throw new AccessDeniedException("You can only update users with the same sector");
+        }
+    }
+
 }
