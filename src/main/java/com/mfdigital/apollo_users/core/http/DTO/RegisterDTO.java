88315af24775d8dc7bf881547977c890fc84cf14
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-05 18:12:19 -0300

    FEAT: exception handler

--- src/main/java/com/mfdigital/apollo_users/core/http/DTO/RegisterDTO.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/DTO/RegisterDTO.java
@@ -8,9 +8,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
+
 public record RegisterDTO(
         @NotBlank(message = "Name cannot be blank")
-        @Pattern(regexp = "^[A-Za-z]+$", message = "Please, enter your name correctly.")
+        @Pattern(
+                regexp = "^[A-Za-z]+$", message = "Please, enter your name correctly."
+        )
         String name,
 
         @NotBlank(message = "Last name cannot be blank")
@@ -18,12 +21,15 @@ public record RegisterDTO(
         String lastName,
 
         @NotBlank(message = "Email cannot be blank")
-        @Email(message = "Email must be valid and end with '@meirelesefreitas.adv.br'")
-        @Pattern(regexp = "^[A-Za-z0-9.]+@meirelesefreitas\\.adv\\.br$", message = "Email must end with '@meirelesefreitas.adv.br'.")
+        @Email
+        @Pattern(regexp = "^[A-Za-z0-9.]+@meirelesefreitas\\.adv\\.br$", message = "Email must be valid and end with '@meirelesefreitas.adv.br'")
         String email,
 
         @NotBlank(message = "Password cannot be blank")
-        @Pattern(regexp = "^[A-Za-z0-9._%+#$&*@!?-]+$", message = "Password must contain only allowed characters")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+                message = "Password must contain at least one lowercase character, one uppercase character, one special character, and must be at least 8 characters long"
+        )
         String password,
 
         @NotNull(message = "Role cannot be null")
