7781668dbd9f768ce8c7a0717021fa2bf8e0d6b7
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-07 13:38:04 -0300

    REFACTOR: loginDTO to loginResponseDTO

--- src/main/java/com/mfdigital/apollo_users/core/http/DTO/LoginResponseDTO.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/DTO/LoginResponseDTO.java
@@ -4,5 +4,5 @@ import com.mfdigital.apollo_users.core.entity.enums.Sector;
 import com.mfdigital.apollo_users.core.entity.enums.State;
 import com.mfdigital.apollo_users.core.entity.enums.UserRole;
 
-public record LoginDTO(String token, String username, UserRole role, Sector sector, State state) {
+public record LoginResponseDTO(String token, String username, UserRole role, Sector sector, State state) {
 }
--- src/main/java/com/mfdigital/apollo_users/core/http/controllers/AuthController.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/controllers/AuthController.java
@@ -2,7 +2,7 @@ package com.mfdigital.apollo_users.core.http.controllers;
 
 import com.mfdigital.apollo_users.core.entity.User;
 import com.mfdigital.apollo_users.core.http.DTO.AuthDTO;
-import com.mfdigital.apollo_users.core.http.DTO.LoginDTO;
+import com.mfdigital.apollo_users.core.http.DTO.LoginResponseDTO;
 import com.mfdigital.apollo_users.core.http.DTO.RegisterDTO;
 import com.mfdigital.apollo_users.core.http.services.TokenService;
 import com.mfdigital.apollo_users.core.repositories.UserRepository;
@@ -38,7 +38,7 @@ public class AuthController {
             var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
             var auth = this.authenticationManager.authenticate(usernamePassword);
             var token = tokenService.generateToken((User) auth.getPrincipal());
-            return ResponseEntity.ok(new LoginDTO(token,
+            return ResponseEntity.ok(new LoginResponseDTO(token,
                     user.getUsername(),
                     user.getUserRole(),
                     user.getSector(),
