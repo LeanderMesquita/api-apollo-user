4b5b87b72798e310672c259e4541ca991606d837
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-08-09 11:54:26 -0300

    FEAT: .env configurations

--- src/main/java/com/mfdigital/apollo_users/core/http/config/CorsConfig.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.mfdigital.apollo_users.core.http.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,13 +10,20 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${allowed.ip1}")
+    private String allowedIp1;
+    @Value("${allowed.ip2}")
+    private String allowedIp2;
+
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://192.168.3.113:3000");
+        config.addAllowedOrigin(allowedIp1);
+        config.addAllowedOrigin(allowedIp2);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
