196801faf7dd864bc10bae5033a30b61473a7911
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2024-08-19 12:30:50 -0300

    feat: added to configuration rabbitmq

---
+++ src/main/java/com/mfdigital/apollo_users/core/http/config/UserEventPublisher.java
@@ -0,0 +1,19 @@
+package com.mfdigital.apollo_users.core.http.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mfdigital.apollo_users.core.entity.User;
+
+public class UserEventPublisher {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private TopicExchange userExchange;
+
+    public void publishAdminCreated(User user) {
+        rabbitTemplate.convertAndSend(userExchange.getName(), "user", user);
+    }
+}
