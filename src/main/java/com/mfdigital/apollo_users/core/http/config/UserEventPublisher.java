5b6cf1047820132c41d79a5a6e9dfd6373a79a34
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2024-08-20 14:17:21 -0300

    fix: rabbitmq

--- src/main/java/com/mfdigital/apollo_users/core/http/config/UserEventPublisher.java
+++ src/main/java/com/mfdigital/apollo_users/core/http/config/UserEventPublisher.java
@@ -3,9 +3,11 @@ package com.mfdigital.apollo_users.core.http.config;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mfdigital.apollo_users.core.entity.User;
 
+@Service
 public class UserEventPublisher {
 
     @Autowired
